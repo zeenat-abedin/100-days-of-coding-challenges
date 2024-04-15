4eafa6cb7d141014787251db64d5501759a5cb5c
zeenat <zeenu.23@gmail.com>
2024-04-15 13:04:19 +0530

    add alternative implementation of sumOfSquares

--- Day15/script.js
+++ Day15/script.js
@@ -7,4 +7,14 @@ function sumOfSquares(arr) {
     return arr.reduce((acc, curr) => (acc = acc + curr * curr), 0)
 }
 
-console.log(sumOfSquares([0,1,2,3,4]))
+console.log(sumOfSquares([0, 1, 2, 3, 4]))
+
+function sumOfSqs(arr) {
+    let sum = 0
+    for (const iterator of arr) {
+        sum = sum + iterator * iterator
+    }
+    return sum
+}
+
+console.log(sumOfSqs([0, 1, 2, 3, 4]))
