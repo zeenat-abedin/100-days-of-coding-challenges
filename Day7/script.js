dfcc6030315a16b7ac22d3def9528bd940da0c4d
zeenat <zeenu.23@gmail.com>
2024-04-23 12:49:17 +0530

    day7: findMax

---
+++ Day7/script.js
@@ -0,0 +1,7 @@
+// 7. Write a function findMax that takes an array and returns the highest number in the array.
+
+function findMax(arr) {
+    return Math.max(...arr)
+}
+
+console.log(findMax([1, 2, 3,8,5,4]));
