95f86dc29424c05547806d5755a000cc80ec46fe
zeenat <zeenu.23@gmail.com>
2024-04-23 11:23:12 +0530

    day9: factorial

---
+++ Day8/script.js
@@ -0,0 +1,12 @@
+// 8. Write a function factorial that takes a non-negative integer num as input, and returns its factorial.
+
+function factorial(num) {
+    if (num === 0) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
+}
+
+console.log(factorial(5));
+
