0c5773b9010dcaf384d07c89658f081b2117119f
zeenat <zeenu.23@gmail.com>
2024-05-02 00:56:23 +0530

    day1; mean

---
+++ Day1/script.js
@@ -0,0 +1,14 @@
+// 1. Calculate mean of the array
+// 2. Calculate median of the array
+// 3. Calculate mode of the arrays
+
+function calculateMean(arr) {
+    if (arr.length === 0) return 0
+    let sum = arr.reduce((acc, curr) => acc + curr, 0)
+    return sum / arr.length
+}
+
+console.log(calculateMean([1,2,3,4,5]))
+console.log(calculateMean([10,20,30]))
+console.log(calculateMean([-1,0,1]))
+console.log(calculateMean([])) //empty array should return 0
