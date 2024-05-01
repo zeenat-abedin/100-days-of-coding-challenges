f0950932b0a05f59f40ec780ce5a9cdd3574d3ff
zeenat <zeenu.23@gmail.com>
2024-05-02 00:00:14 +0530

    day3: countChar

--- Day3/script.js
+++ Day3/script.js
@@ -1,8 +1,17 @@
 // 3. Write a function called countChar that takes two parameters: a string and a character to count. The function should return the number of times the specified character appears in the string.
 
 
-function countChar() {
-    
+function countChar(word, char){
+    word = word.toLowerCase()
+    char = char.toLowerCase()
+
+    totalCount = word.split("").reduce((acc, currChar) => {
+        if(currChar === char){
+            acc++
+        }
+        return acc
+    }, 0)
+    return totalCount
 }
 
-console.log("MissIssIppi", "I")
+console.log(countChar("MissIssIppi", "I"))
