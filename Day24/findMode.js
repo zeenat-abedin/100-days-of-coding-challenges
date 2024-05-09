215be436ea70b7592df98175ab4eb5dab29d5adc
zeenat <zeenu.23@gmail.com>
2024-05-09 16:05:59 +0530

    update findMode function to return the mode of an array of numbers

--- Day24/findMode.js
+++ Day24/findMode.js
@@ -2,9 +2,20 @@
 
 
 function findMode(arr) {
-    
+    let counts = {}
+    let maxNum = 0
+    let mode;
+
+    for (let element of arr) {
+    counts[element] = (counts[element] || 0) + 1
+        if (counts[element] > maxNum) {  
+            maxNum = counts[element]  
+            mode = maxNum
+      }
+   }
+    return mode   
 }
 
-console.log(findMode([1,2,2,3,4,2]))
-// console.log(findMode())
-// console.log(findMode())
+console.log(findMode([1, 2, 2, 3, 4, 2])) //Output: 2
+
+//Note: The mode is the most common number that appears in your set of data. To find the mode count how often each number appears and the number that appears the most times is the mode.
