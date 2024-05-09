6064fa0ed01d049b46ffe7454440039551af19f4
zeenat <zeenu.23@gmail.com>
2024-05-09 08:45:57 +0530

    day24: findMode

---
+++ Day24/findMode.js
@@ -0,0 +1,10 @@
+//24. Write a function called findMode that takes an array of numbers as input and returns the mode of the array (the numbers that appears most frequently)
+
+
+function findMode(arr) {
+    
+}
+
+console.log(findMode([1,2,2,3,4,2]))
+// console.log(findMode())
+// console.log(findMode())
