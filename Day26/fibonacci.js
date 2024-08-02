9fc99d57f6cb8e349c4a74db6f7f281882ce13f9
zeenat <zeenu.23@gmail.com>
2024-08-02 21:01:06 +0530

    fibonacci

--- Day26/fibonacci.js
+++ Day26/fibonacci.js
@@ -1,7 +1,11 @@
 // fibonacci using recursion
 
+//The fibonacci series is a sequence of numbers in which each number (fibonacci number) is the 
+
 function fibonacci(num) {
     
 }
 
+console.log(fibonacci(2))
+console.log(fibonacci(3))
 console.log(fibonacci(4))
