cf7baaa73d5ba530e50eeea697530cd3120d031c
zeenat <zeenu.23@gmail.com>
2024-08-02 21:54:30 +0530

    implement recursive Fibonacci function

--- Day26/fibonacci.js
+++ Day26/fibonacci.js
@@ -1,9 +1,13 @@
 // fibonacci using recursion
 
-//The fibonacci series is a sequence of numbers in which each number (fibonacci number) is the 
+//The fibonacci series is a sequence of numbers in which each number (fibonacci number) is the sum of two preceding numbers
 
 function fibonacci(num) {
-    
+   if (num <= 1) {
+     return num
+    } else {
+       return fibonacci(num - 1) + fibonacci(num - 2)
+    }      
 }
 
 console.log(fibonacci(2))
