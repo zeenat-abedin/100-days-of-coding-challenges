9d7193a0d7c59003f996cf62c1353ec7e7cb6b36
zeenat <zeenu.23@gmail.com>
2024-08-12 23:36:08 +0530

    implement string truncation logic

--- Day28/truncateString.js
+++ Day28/truncateString.js
@@ -1,7 +1,9 @@
 // Write a function that takes two parameters: a str and an integer representing the max length.
 
-function truncateString(str) {
-    
+function truncateString(str, count) {
+    if (str.length > count) {
+        return str.slice(0, count)
+    }
 }
 
 console.log(truncateString('A-tisket a-tasket a green and a red flag', 8)) //Output -> A-tisket...
