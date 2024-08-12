1218acbfcbbf54e1a2f8b59509035d1f36e9b319
zeenat <zeenu.23@gmail.com>
2024-08-12 23:22:41 +0530

    day28: truncateString

---
+++ Day28/truncateString.js
@@ -0,0 +1,7 @@
+// Write a function that takes two parameters: a str and an integer representing the max length.
+
+function truncateString(str) {
+    
+}
+
+console.log(truncateString('A-tisket a-tasket a green and a red flag', 8))
