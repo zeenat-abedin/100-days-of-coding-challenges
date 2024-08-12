a58bccdc688d2bb4b852dea2ad601609d4ec3d56
zeenat <zeenu.23@gmail.com>
2024-08-12 23:11:44 +0530

    update repeatString function to return original string when num is 0

--- Day27/repeatString.js
+++ Day27/repeatString.js
@@ -1,8 +1,8 @@
 // Write a function called repeatString that takes in 2 parameters
 
 function repeatString(str, num) {
-    return num > 0 ? str.repeat(num) : ''
+    return num > 0 ? str.repeat(num) : str
 }
 
 
-console.log(repeatString('abc', 5)) // Output-> 'abcabcabcabcabc'
+console.log(repeatString('abc', 3)) // Output-> 'abcabcabcabcabc'
