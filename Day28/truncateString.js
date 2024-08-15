292d58a43d3dd92855a0415903ac5b095b7bd740
zeenat <zeenu.23@gmail.com>
2024-08-15 22:15:55 +0530

    change argument

--- Day28/truncateString.js
+++ Day28/truncateString.js
@@ -13,6 +13,6 @@ const truncatedString = (str, count) => {
     return count < 0 ? str : str.slice(0, count).concat('...')
 }
 
-console.log(truncateString('A-tisket a-tasket a green and a red flag', 6)) //Output -> A-tisket...
+console.log(truncateString('A-tisket a-tasket a green and a red flag', 5)) //Output -> A-tisket...
 
 console.log(truncatedString('A-tisket a-tasket a green and a red flag', 8)) 
