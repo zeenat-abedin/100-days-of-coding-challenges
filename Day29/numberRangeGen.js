d1d80a19a39ae821fc7192394c76912955244858
zeenat <zeenu.23@gmail.com>
2024-08-20 00:21:53 +0530

    add output comments to numberRangeGen.js

--- Day29/numberRangeGen.js
+++ Day29/numberRangeGen.js
@@ -9,5 +9,5 @@ const numberRange = (a,b) => {
 }
 
 console.log(numberRange(0,5)) // Output -> [1, 2, 3, 4,  5]
-console.log(numberRange(3,7))
-console.log(numberRange(-2,4))
+console.log(numberRange(3,7)) // Output -> [ 3, 4, 5, 6, 7 ]
+console.log(numberRange(-2,4)) // Output -> [-2, -1, 0, 1,2,  3, 4]
