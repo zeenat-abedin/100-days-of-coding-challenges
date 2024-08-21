f20816e924f14c88512d5ff39508c9e51177d61b
zeenat <zeenu.23@gmail.com>
2024-08-21 21:04:54 +0530

    add implementation of numberRangeRecursive function and test cases

--- Day30/numberRangeRecursive.js
+++ Day30/numberRangeRecursive.js
@@ -1,1 +1,13 @@
 // Write a recursive function called numberRangeRecursive that generates an array containing consecutive numbers from a to b (inclusive).
+
+function numberRangeRecursive(a,b, arr = []) {
+    if (a<=b) {
+        arr.push(a)
+        return numberRangeRecursive(a+1, b, arr)
+    }
+    return arr
+}
+
+console.log(numberRangeRecursive(0,5))
+console.log(numberRangeRecursive(3,7))
+console.log(numberRangeRecursive(-2,2))
