4563b8ebfead6534d9da3a3aae4fc91be4cbdb44
zeenat <zeenu.23@gmail.com>
2024-09-07 18:38:41 +0530

    calculateSimpleInterest func. declaration

---
+++ Day37/calculateSimpleInterest.js
@@ -0,0 +1,7 @@
+// Write a function called calculateSimpleInterest that calculates the simple interest given the principal amount, rate of interest per annum, and time in years.
+
+function calculateSimpleInterest(principal, rate, time) {
+    
+}
+
+console.log(calculateSimpleInterest(1000, 5, 3)) //Output -> 150
