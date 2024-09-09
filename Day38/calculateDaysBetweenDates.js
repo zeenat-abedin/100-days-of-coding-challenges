2a6d1eada286f9fa174bab217db08d056d00a7af
zeenat <zeenu.23@gmail.com>
2024-09-09 22:27:32 +0530

    calculateDaysBetweenDates

---
+++ Day38/calculateDaysBetweenDates.js
@@ -0,0 +1,7 @@
+// Dates willl be provided in the format "YYYY-MM-DD"
+
+function calculateDaysBetweenDates() {
+    
+}
+
+console.log(calculateDaysBetweenDates("2024-01-01", "2024-01-2031")) //Output -> 30
