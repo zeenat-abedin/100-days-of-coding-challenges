e41a37893b76da14651aa118938eb7508b07d1b5
zeenat <zeenu.23@gmail.com>
2024-09-10 19:30:48 +0530

    add date obj

--- Day38/calculateDaysBetweenDates.js
+++ Day38/calculateDaysBetweenDates.js
@@ -1,7 +1,8 @@
 // Dates willl be provided in the format "YYYY-MM-DD"
 
-function calculateDaysBetweenDates() {
-    
+const calculateDaysBetweenDates = (d1, d2) => {
+    let currDate = new Date()
+    console.log(currDate.toLocaleDateString())
 }
 
 console.log(calculateDaysBetweenDates("2024-01-01", "2024-01-2031")) //Output -> 30
